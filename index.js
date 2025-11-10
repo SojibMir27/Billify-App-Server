9bb5aefd7c695128aab4cf418f7fb6623c30ac00
SojibMir27 <mdsojibmir2027@gmail.com>
2025-11-10 15:00:03 +0600

    added all setup file and conneted server to MongoDB

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://simpleDBUser:rbVpL7aju8LYd46Q@cluster0.u7ffvmg.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Bilify Server is Running Now!");
+});
+
+app.listen(port, () => {
+  console.log(`Bilify app listening on port ${port}`);
+});
