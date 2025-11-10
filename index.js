3b887b2d65183950f655a97217c2f69d33788090
SojibMir27 <mdsojibmir2027@gmail.com>
2025-11-11 01:49:56 +0600

    added create bills api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -19,8 +19,51 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+  console.log("hlw world!");
   try {
     await client.connect();
+
+    const db = client.db("billify-db");
+    const billCollection = db.collection("bills");
+    const myBillCollection = db.collection("my-bills");
+
+    // bills apis
+    app.get("/bills", async (req, res) => {
+      const result = await billCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/bills/:id", async (req, res) => {
+      const { id } = req.params;
+      const objId = { _id: new ObjectId(id) };
+      const result = await billCollection.findOne(objId);
+      res.send(result);
+    });
+
+    // my-bills apis
+    app.post("/my-bills", async (req, res) => {
+      const data = req.body;
+      const result = await myBillCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/my-bills", async (req, res) => {
+      const result = await myBillCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/my-bills", async (req, res) => {
+      const email = req.query.email;
+      const result = await myBillCollection.find({ email }).toArray();
+      res.send(result);
+    });
+
+    app.get("/my-bills/:id", async (req, res) => {
+      const { id } = req.params;
+      const objId = { _id: new ObjectId(id) };
+      const result = await myBillCollection.findOne(objId);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("You successfully connected to MongoDB!");
