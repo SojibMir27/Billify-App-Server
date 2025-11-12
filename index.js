360b85610c02abfa96ee43011b704adccf109d1f
SojibMir27 <mdsojibmir2027@gmail.com>
2025-11-12 21:04:45 +0600

    added env file and secure username & password

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://simpleDBUser:rbVpL7aju8LYd46Q@cluster0.u7ffvmg.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u7ffvmg.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
